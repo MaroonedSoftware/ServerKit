6fd4e49d2a2000f00b89159982ea442eb19134d2
Robert Dean <99672719+robert-dean@users.noreply.github.com>
2026-04-03 11:32:58 -0400

    Update bigint.ts

--- packages/utilities/src/bigint.ts
+++ packages/utilities/src/bigint.ts
@@ -1,11 +1,11 @@
-export const bigIntReplacer = (_: string, value: any): any => {
+export const bigIntReplacer = (_: string, value: unknown): unknown => {
   if (typeof value === 'bigint') {
     return value.toString() + 'n';
   }
   return value;
 };
 
-export const bigIntReviver = (_: string, value: any): any => {
+export const bigIntReviver = (_: string, value: unknown): unknown => {
   if (typeof value === 'string' && /^-?\d+n$/.test(value)) {
     return BigInt(value.slice(0, -1));
   }
